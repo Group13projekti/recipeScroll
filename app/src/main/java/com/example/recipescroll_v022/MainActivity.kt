2dc5e2032638a3d0988a5b2a4b5134b14cc71f7f
biist <72079769+JanneOhtonen@users.noreply.github.com>
2023-03-22 13:52:00 +0200

    updating login/signup + fragment structure

--- app/src/main/java/com/example/recipescroll_v022/MainActivity.kt
+++ app/src/main/java/com/example/recipescroll_v022/MainActivity.kt
@@ -7,20 +7,21 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+
+    private lateinit var navControllerLogin: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        navControllerLogin = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navControllerLogin)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navControllerLogin.navigateUp() || super.onSupportNavigateUp()
     }
 }
