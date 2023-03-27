1533f96fdbe683dd885bedb105adaf1cfcf7d162
LeeviIlli <t1ille00@students.oamk.fi>
2023-03-27 12:54:00 +0300

    käyttäjän muistaminen kirjautuessa

--- app/src/main/java/com/example/recipescroll_v022/HomeFragment.kt
+++ app/src/main/java/com/example/recipescroll_v022/HomeFragment.kt
@@ -1,12 +1,17 @@
 package com.example.recipescroll_v022
 
+
+import android.content.ContentValues.TAG
+import android.content.*
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 
 
 class HomeFragment : Fragment() {
@@ -15,6 +20,11 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null) {
+            goFeedActivity()
+        }
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val signinbutton = view.findViewById<Button>(R.id.signinbtn)
@@ -28,6 +38,13 @@ class HomeFragment : Fragment() {
         }
 
         return view
+    }
+
+
+    fun goFeedActivity() {
+        val intent = Intent(activity, FeedActivity::class.java)
+        startActivity(intent)
+        activity?.finish()
     }
 
 }
