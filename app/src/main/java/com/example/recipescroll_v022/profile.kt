5d0368792274f48f5b494d528dcc87473d5db028
LeeviIlli <t1ille00@students.oamk.fi>
2023-03-30 12:45:08 +0300

    käyttäjänimi, nimi ja bio saadaan databasesta

--- app/src/main/java/com/example/recipescroll_v022/profile.kt
+++ app/src/main/java/com/example/recipescroll_v022/profile.kt
@@ -5,54 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [profile.newInstance] factory method to
- * create an instance of this fragment.
- */
 class profile : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var usernameTextView: TextView
+    private lateinit var nameTextView: TextView
+    private lateinit var bioTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+        usernameTextView = view.findViewById(R.id.usernameTextView)
+        nameTextView = view.findViewById(R.id.nameTextView)
+        bioTextView = view.findViewById(R.id.bioTextView)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            profile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onStart() {
+        super.onStart()
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user != null) {
+            val db = Firebase.firestore
+            db.collection("users").document(user.uid)
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val username = document.getString("uname")
+                        usernameTextView.text = username
+                        val name = document.getString("name")
+                        nameTextView.text = name
+                        val bio = document.getString("bio")
+                        bioTextView.text = bio
+                    }
                 }
-            }
+                .addOnFailureListener { exception ->
+                    // handle exception
+                }
+        }
     }
 }
