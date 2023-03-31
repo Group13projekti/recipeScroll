4d3f0029a2c4eb28386848330e0b2da4bb4f2d5d
biist <72079769+JanneOhtonen@users.noreply.github.com>
2023-03-31 11:51:44 +0300

    + updated TAG + fixed going to login page after logging in or signing up when pressing back + added intent in sign up

--- app/src/main/java/com/example/recipescroll_v022/FeedActivity.kt
+++ app/src/main/java/com/example/recipescroll_v022/FeedActivity.kt
@@ -2,10 +2,7 @@ package com.example.recipescroll_v022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
