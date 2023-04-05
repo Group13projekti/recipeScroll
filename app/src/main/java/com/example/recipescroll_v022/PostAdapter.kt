07fa6f3e608016eaf8c256e07978e67b3951d4fb
LeeviIlli <t1ille00@students.oamk.fi>
2023-04-05 11:49:21 +0300

    Profiilin tekoa profiilikuva ei näy

--- app/src/main/java/com/example/recipescroll_v022/PostAdapter.kt
+++ app/src/main/java/com/example/recipescroll_v022/PostAdapter.kt
@@ -10,8 +10,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recipescroll_v022.models.PostDB
 
-
-
 class PostAdapter(val context: FrontPage, val posts: List <PostDB>) :
     RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
@@ -30,8 +28,20 @@ class PostAdapter(val context: FrontPage, val posts: List <PostDB>) :
         val currItem = posts[position]
         holder.username.text = currItem.user?.uname
         Glide.with(context).load(currItem.imageUrl).into(holder.Post)
+        Glide.with(context).load(currItem.user?.profileImageUrl).into(holder.profileImage)
         holder.relativetime.text = DateUtils.getRelativeTimeSpanString(currItem.creationTime)
         holder.description.text = currItem.description
+
+        if (currItem.user?.profileImageUrl != null) {
+            Glide.with(context)
+                .load(currItem.user!!.profileImageUrl)
+                .placeholder(R.drawable.placeholder_image) // asetetaan profiilikuvan sijaan placeholder-image
+                .into(holder.profileImage)
+        } else {
+            Glide.with(context)
+                .load(R.drawable.placeholder_image) // asetetaan placeholder-image, jos profiilikuvaa ei ole
+                .into(holder.profileImage)
+        }
     }
 
     inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
@@ -39,7 +49,10 @@ class PostAdapter(val context: FrontPage, val posts: List <PostDB>) :
         val username : TextView = itemView.findViewById(R.id.tvUsername)
         val relativetime : TextView = itemView.findViewById(R.id.tvRelativetime)
         val Post : ImageView = itemView.findViewById(R.id.ivPost)
+        val profileImage : ImageView = itemView.findViewById(R.id.profile_image)
         val description : TextView = itemView.findViewById(R.id.tvDescription)
+
+
 
     }
 }
--- app/src/main/java/com/example/recipescroll_v022/models/UserDB.kt
+++ app/src/main/java/com/example/recipescroll_v022/models/UserDB.kt
@@ -1,6 +1,11 @@
 package com.example.recipescroll_v022.models
 
-data class UserDB(var name : String? = null,
-                  val uname : String? = null,
-                  val email: String? = null
+import com.google.firebase.firestore.PropertyName
+
+data class UserDB(
+    var name : String? = null,
+    val uname : String? = null,
+    val email: String? = null,
+    @get:PropertyName("profileImageUrl") @set:PropertyName("profileImageUrl")
+    var profileImageUrl: String = "",
 )
