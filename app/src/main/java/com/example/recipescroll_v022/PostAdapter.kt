6e0707516a565e09739e50898c494633328afeaa
eduvaa <eemiliki@gmail.com>
2023-04-20 15:47:25 +0300

    favorite deletattu ja muutettu like like napiksi

--- app/src/main/java/com/example/recipescroll_v022/PostAdapter.kt
+++ app/src/main/java/com/example/recipescroll_v022/PostAdapter.kt
@@ -4,33 +4,47 @@ import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recipescroll_v022.models.PostDB
+import com.example.recipescroll_v022.models.UserDB
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
 
-class PostAdapter(val context: FrontPage, val posts: List <PostDB>) :
+class PostAdapter(val context: FrontPage, val posts: List<PostDB>) :
     RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
-
+    private val favoriteStates = mutableMapOf<String, Boolean>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
-        return ViewHolder(view)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_post, parent, false)
+
+        val favButton = view.findViewById<CheckBox>(R.id.cb_favorites)
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val userDocRef = FirebaseFirestore.getInstance().collection("users").document(userId!!)
+        var favorites = emptyList<String>()
+
+        return ViewHolder(view, favButton, userDocRef, favorites)
     }
 
     override fun getItemCount() = posts.size
 
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
         val currItem = posts[position]
         holder.username.text = currItem.user?.uname
         Glide.with(context).load(currItem.imageUrl).into(holder.Post)
         Glide.with(context).load(currItem.user?.profileImageUrl).into(holder.profileImage)
         holder.relativetime.text = DateUtils.getRelativeTimeSpanString(currItem.creationTime)
         holder.description.text = currItem.description
+
+        val isFavorite = favoriteStates[currItem.postId] ?: false
+        holder.favButton.isChecked = isFavorite
+
 
         if (currItem.user?.profileImageUrl != null) {
             Glide.with(context)
@@ -44,15 +58,72 @@ class PostAdapter(val context: FrontPage, val posts: List <PostDB>) :
         }
     }
 
-    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class ViewHolder(
+        itemView: View,
+        val favButton: CheckBox,
+        private val userDocRef: DocumentReference,
+        private var favorites: List<String>
+    ) : RecyclerView.ViewHolder(itemView) {
 
-        val username : TextView = itemView.findViewById(R.id.tvUsername)
-        val relativetime : TextView = itemView.findViewById(R.id.tvRelativetime)
-        val Post : ImageView = itemView.findViewById(R.id.ivPost)
-        val profileImage : ImageView = itemView.findViewById(R.id.profile_image)
-        val description : TextView = itemView.findViewById(R.id.tvDescription)
+        val username: TextView = itemView.findViewById(R.id.tvUsername)
+        val relativetime: TextView = itemView.findViewById(R.id.tvRelativetime)
+        val Post: ImageView = itemView.findViewById(R.id.ivPost)
+        val profileImage: ImageView = itemView.findViewById(R.id.profile_image)
+        val description: TextView = itemView.findViewById(R.id.tvDescription)
 
+        init {
+            userDocRef.get().addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = task.result?.toObject(UserDB::class.java)
+                    favorites = user?.favorites ?: emptyList()
+                    favButton.isChecked = favorites.contains(posts[adapterPosition].postId)
 
+                    favButton.setOnCheckedChangeListener { _, isChecked ->
+                        val currItem = posts[adapterPosition]
+                        val post_id = currItem.postId
+                        val userId = FirebaseAuth.getInstance().currentUser?.uid
+                        val userDocRef = FirebaseFirestore.getInstance().collection("users").document(userId!!)
+                        userDocRef.get().addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val user = task.result?.toObject(UserDB::class.java)
+                                var mutableFavorites = user?.favorites?.toMutableList() ?: mutableListOf()
+                                if (isChecked) {
+                                    if (!mutableFavorites.contains(post_id)) {
+                                        // Add the post to the user's favorites list in Firebase
+                                        mutableFavorites.add(post_id)
+                                        favorites = mutableFavorites
+                                        userDocRef.update("favorites", favorites)
+                                    }
+                                } else {
+                                    if (mutableFavorites.contains(post_id)) {
+                                        // Remove the post from the user's favorites list in Firebase
+                                        mutableFavorites.remove(post_id)
+                                        favorites = mutableFavorites
+                                        userDocRef.update("favorites", favorites)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
+        fun bind(post: PostDB) {
+            username.text = post.user?.uname
+            Glide.with(context).load(post.imageUrl).into(Post)
+            Glide.with(context).load(post.user?.profileImageUrl).into(profileImage)
+            relativetime.text = DateUtils.getRelativeTimeSpanString(post.creationTime)
+            description.text = post.description
+
+            // Check if the post is already favorited
+            if (favorites.contains(post.postId)) {
+                favButton.isChecked = true
+                favButton.text = "Favorited"
+            } else {
+                favButton.isChecked = false
+                favButton.text = "Add to Favorites"
+            }
+        }
     }
-}
+    }
--- app/src/main/java/com/example/recipescroll_v022/models/PostDB.kt
+++ app/src/main/java/com/example/recipescroll_v022/models/PostDB.kt
@@ -3,6 +3,7 @@ package com.example.recipescroll_v022.models
 import com.google.firebase.firestore.PropertyName
 
 data class PostDB(
+    @get:PropertyName("post_id") @set:PropertyName("post_id") var postId: String = "",
     var description: String = "",
     @get:PropertyName("image_url") @set:PropertyName("image_url") var imageUrl: String = "",
     @get:PropertyName("creation_time_ms") @set:PropertyName("creation_time_ms") var creationTime: Long = 0,
